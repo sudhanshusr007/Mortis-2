a2ce5818f1336e50fa85c4f3ad67b115ce1f8023
Sudhanshu Ranjan <sudhanshusr007@gmail.com>
2024-07-01 19:55:59 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ config({path:"./config/config.env"});
 
 app.use(cors(
     {
-        origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL,'*'],
+        origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL,'http://localhost:5173'],
         methods:["GET","POST","PUT","DELETE"],
         credentials:true,
     }
